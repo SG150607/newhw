977d6ef2e642bbf48e35b7f296e355b1b8e9b41c
dodiki2.0 <SabinaGasymova150607@gmail.com>
2023-01-21 21:17:31 +0300

    getting the program

---
+++ main.py
@@ -0,0 +1,39 @@
+import random
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QColor
+from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
+
+
+class Example(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.draw_circle = False
+        self.pushButton.clicked.connect(self.draw)
+
+    def paintEvent(self, event):
+        if self.draw_circle:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def draw(self):
+        self.draw_circle = True
+        self.repaint()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        sizee = random.randint(10, 100)
+        qp.drawEllipse(random.randint(0, 570), random.randint(0, 450), sizee, sizee)
+
+        self.draw_circle = False
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Example()
+    ex.show()
+    sys.exit(app.exec())
