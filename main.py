2541457ef33c4df7fb36ccbdd4d5641fd28b31eb
dodiki2.0 <SabinaGasymova150607@gmail.com>
2023-01-21 21:26:09 +0300

    a bit change to the program

--- main.py
+++ main.py
@@ -3,15 +3,23 @@ import sys
 
 from PyQt5 import uic
 from PyQt5.QtGui import QPainter, QColor
-from PyQt5.QtWidgets import QApplication, QWidget, QMainWindow, QPushButton
+from PyQt5.QtWidgets import QApplication, QWidget, QPushButton
 
 
-class Example(QMainWindow):
+class Example(QWidget):
     def __init__(self):
         super().__init__()
-        uic.loadUi('UI.ui', self)
+        self.initUI()
+
+    def initUI(self):
+        self.setGeometry(100, 100, 570, 450)
+        self.setWindowTitle('Git и случайные окружности')
+
+        self.check_button = QPushButton(self)
+        self.check_button.setText('Нарисовать')
+        self.check_button.setGeometry(220, 340, 115, 40)
         self.draw_circle = False
-        self.pushButton.clicked.connect(self.draw)
+        self.check_button.clicked.connect(self.draw)
 
     def paintEvent(self, event):
         if self.draw_circle:
@@ -25,10 +33,9 @@ class Example(QMainWindow):
         self.repaint()
 
     def draw_flag(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
+        qp.setBrush(QColor(random.randint(0, 255), random.randint(0, 255), random.randint(0, 255)))
         sizee = random.randint(10, 100)
         qp.drawEllipse(random.randint(0, 570), random.randint(0, 450), sizee, sizee)
-
         self.draw_circle = False
 
 
